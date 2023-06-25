cbbc176125f403a634eaeefa2d85e1fb46791ce0
zgp <853157420@qq.com>
2023-06-25 20:15:01 +0800

    hot-fix 2

--- src/com/zgp/GitTest.java
+++ src/com/zgp/GitTest.java
@@ -1,8 +1,13 @@
 package com.zgp;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 /**
  * @author 周光璞
  * @creation_time 2023-06-25-20:09
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
 }
